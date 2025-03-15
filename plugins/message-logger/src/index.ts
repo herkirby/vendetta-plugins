be70e000c0d9215f7b712128efe16249fda19cbc
herkirby <zachary.price6980@gmail.com>
2025-03-15 13:49:30 -0400

    Update index.ts

--- plugins/message-logger/src/index.ts
+++ plugins/message-logger/src/index.ts
@@ -1,6 +1,9 @@
 import { findByName, findByProps } from "@vendetta/metro";
 import { FluxDispatcher, ReactNative } from "@vendetta/metro/common";
 import { after, before, instead } from "@vendetta/patcher";
+import { storage } from "@vendetta/plugin";
+import { writeFile, existsSync, mkdirSync } from "react-native-fs";
+import { join } from "path";
 
 const patches = [];
 const ChannelMessages = findByProps("_channelMessages");
@@ -8,7 +11,10 @@ const MessageRecordUtils = findByProps("updateMessageRecord", "createMessageReco
 const MessageRecord = findByName("MessageRecord", false);
 const RowManager = findByName("RowManager");
 
-import { storage } from "@vendetta/plugin";
+const logDirectory = join(ReactNative.Platform.OS === "android" ? ReactNative.fs.ExternalDirectoryPath : ReactNative.fs.DocumentDirectoryPath, "deleted_messages");
+if (!existsSync(logDirectory)) {
+  mkdirSync(logDirectory);
+}
 
 patches.push(before("dispatch", FluxDispatcher, ([event]) => {
   if (event.type === "MESSAGE_DELETE") {
@@ -33,6 +39,18 @@ patches.push(before("dispatch", FluxDispatcher, ([event]) => {
           });
         }
       });
+
+    const logFilePath = join(logDirectory, `${event.channelId}.json`);
+    const logEntry = JSON.stringify({
+      message: message.toJS(),
+      timestamp: Date.now(),
+    }) + ",\n";
+
+    if (existsSync(logFilePath)) {
+      ReactNative.fs.appendFile(logFilePath, logEntry, "utf8");
+    } else {
+      ReactNative.fs.writeFile(logFilePath, "[" + logEntry, "utf8");
+    }
 
     return [{
       message: {
@@ -63,18 +81,24 @@ patches.push(instead("updateMessageRecord", MessageRecordUtils, function ([oldRe
 
 patches.push(after("createMessageRecord", MessageRecordUtils, function ([message], record) {
   record.__vml_deleted = message.__vml_deleted;
-  // record.__vml_edits = message.__vml_edits;
 }));
 
 patches.push(after("default", MessageRecord, ([props], record) => {
   record.__vml_deleted = !!props.__vml_deleted;
-  // record.__vml_edits = props.__vml_edits;
 }));
 
 export const onUnload = () => {
   patches.forEach((unpatch) => unpatch());
 
   for (const channelId in ChannelMessages._channelMessages) {
+    const logFilePath = join(logDirectory, `${channelId}.json`);
+    if (existsSync(logFilePath)) {
+      const fileContent = ReactNative.fs.readFileSync(logFilePath, 'utf8');
+      if (fileContent.endsWith(",\n")) {
+        const fixedContent = fileContent.slice(0, -2) + "]";
+        ReactNative.fs.writeFile(logFilePath, fixedContent, 'utf8');
+      }
+    }
     for (const message of ChannelMessages._channelMessages[channelId]._array) {
       message.__vml_deleted && FluxDispatcher.dispatch({
         type: "MESSAGE_DELETE",
